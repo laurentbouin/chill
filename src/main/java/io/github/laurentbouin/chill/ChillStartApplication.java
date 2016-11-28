27f25c28332ef5f9ff29ecf98c762a70ad1090f7
Laurent <laurent.bouin@spcapitaliq.com>
2016-11-28 16:43:40 -0500

    initialization

---
+++ src/main/java/io/github/laurentbouin/chill/ChillStartApplication.java
@@ -0,0 +1,12 @@
+package io.github.laurentbouin.chill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChillStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChillStartApplication.class, args);
+	}
+}
