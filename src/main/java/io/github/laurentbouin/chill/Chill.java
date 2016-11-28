bd56bed2a3d587220a28d359cdfa71a05ffd5d21
Laurent <laurent.bouin@spcapitaliq.com>
2016-11-28 17:06:54 -0500

    Starting point

--- src/main/java/io/github/laurentbouin/chill/Chill.java
+++ src/main/java/io/github/laurentbouin/chill/Chill.java
@@ -1,12 +1,14 @@
 package io.github.laurentbouin.chill;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChillStartApplication {
+@Slf4j
+public class Chill {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChillStartApplication.class, args);
+		SpringApplication.run(Chill.class, args);
 	}
 }
